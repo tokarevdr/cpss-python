f58e80da3c3eb9c6a68e50460d91cdcd3f490bbd
tokarevdr <verakotishe@gmail.com>
2025-03-11 17:41:14 +0300

    Сделал отдельную функцию рисования симуляции. Добавил рисование результирующей симуляции

--- src/main.py
+++ src/main.py
@@ -13,18 +13,7 @@ sys.path.append('G:/Мой диск/MyEducation/ИТМО/ВКР/soft/')
 
 import vesradcom as vrc
 from vesradcom.entities import FiniteLengthDipole, Vessel, Satellite, Landmark, Station
-from vesradcom.units import Angle, Frequency, Power, Distance
-
-
-def earth_sphere(r = 1, x0 = 0, y0 = 0, z0 = 0, n = 100, m = 100):
-    u = np.reshape(np.linspace(0, np.pi, n), (n, 1))
-    v = np.reshape(np.linspace(0, 2*np.pi, m), (1, m))
-
-    x = x0 + r * np.sin(u) * np.cos(v)
-    y = y0 + r * np.sin(u) * np.sin(v)
-    z = z0 + r * np.cos(u) * np.ones((1, m))
-
-    return (x, y, z)
+from vesradcom.units import Angle, Frequency, Power, Distance, Velocity
 
 
 def draw_satellite(ax, sat: Satellite, color):
@@ -49,11 +38,25 @@ def draw_station(ax, station: Station):
     ax.plot(*station.coverage_area().exterior.xy, transform=ccrs.PlateCarree(), color='blue')
 
 
+def draw_simulation(ax, sim: vrc.Simulation):
+    ax.plot(*sim.area().exterior.xy, '--', transform=ccrs.PlateCarree(), color='green')
+    draw_vessel(ax, sim.vessel())
+
+    for i in range(sim.satellite_count()):
+        draw_satellite(ax, sim.satellite_at(i), 'red')
+
+    for i in range(sim.landmark_count()):
+        draw_landmark(ax, sim.landmark_at(i))
+
+    for i in range(sim.station_count()):
+        draw_station(ax, sim.station_at(i))
+
+
 def main():
     print("Hello!")
 
     moscow_timezone = pytz.timezone("Europe/Moscow")
-    time = datetime.datetime(2025, 2, 1, 9, 00, 10, 637, moscow_timezone)
+    time = datetime.datetime(2025, 2, 1, 6, 00, 10, 637, moscow_timezone)
 
     tle1 = '''Спутник 1
              1 53385U 22096R   22269.14435733  .00009653  00000-0  37762-3 0  999 5
@@ -63,46 +66,50 @@ def main():
              1 17181U 86096A   24343.04493793  .00000019  00000-0  00000-0 0  9992
              2 17181  13.1563 343.6233 0041591 313.7972 237.2580  0.98925627146461'''
     
-    antenna = FiniteLengthDipole(Frequency(hz=1e+6), Angle(degrees=25), Angle(degrees=-15), Power(w=3.7e-12), Power(w=10))
-    vessel = Vessel(Angle(degrees=40), Angle(degrees=-25), Angle(degrees=60))
+    antenna = FiniteLengthDipole(Frequency(hz=1e+6), Power(w=3.7e-12), Power(w=12), Power(w=10))
+    vessel = Vessel(Angle(degrees=15), Angle(degrees=-48), Angle(degrees=30), Velocity(km_per_s=0.02))
     area = [(-51, 48), (-15, 48), (-20, 12), (-51, 12)]
 
     simulation = vrc.Simulation(area = area, antenna = antenna, vessel = vessel)
 
     simulation.append_satellite(Satellite(tle1, Power(10), 1))
-    simulation.append_satellite(Satellite(tle2, Power(10), 1))
+    simulation.append_satellite(Satellite(tle2, Power(15), 1))
     simulation.set_current_datetime(time)
     simulation.update()
 
-    print('satellite visible:', simulation.satellite_at(0).visible())
-    print('satellite detectable:', simulation.satellite_at(0).detectable())
+    for i in range(simulation.satellite_count()):
+        print(f'satellite {i} visible:', simulation.satellite_at(i).visible())
+        print(f'satellite {i} detectable:', simulation.satellite_at(i).detectable())
+
     
-    fig = plt.figure(figsize=(15, 7))
-    ax2 = fig.add_subplot(1, 1, 1, projection=ccrs.Robinson())
+    fig1 = plt.figure(figsize=(15, 7))
+    ax1 = fig1.add_subplot(1, 1, 1, projection=ccrs.Robinson())
+
+    fig2 = plt.figure(figsize=(15, 7))
+    ax2 = fig2.add_subplot(1, 1, 1, projection=ccrs.Robinson())
 
     shape_files_dir_path = 'G:/Мой диск/MyEducation/ИТМО/ВКР/soft/python/first_steps/10m_physical'
     shape_files = glob(shape_files_dir_path + '/ne_10m_land.shp')
     for shape_file in shape_files:
         shape_feature = ShapelyFeature(Reader(shape_file).geometries(), ccrs.PlateCarree(), facecolor='none', alpha = 0.5)
+        ax1.add_feature(shape_feature)
         ax2.add_feature(shape_feature)
 
+    ax1.set_global()
     ax2.set_global()
     
-    ax2.plot(*simulation.area().exterior.xy, '--', transform=ccrs.PlateCarree(), color='green')
-    draw_vessel(ax2, vessel)
+    draw_simulation(ax1, simulation)
 
-    colors = ['red', 'blue']
-    for i in range(simulation.satellite_count()):
-        draw_satellite(ax2, simulation.satellite_at(i), colors[i])
+    # ax2.plot(*simulation.satellite_at(0).track(time, time + datetime.timedelta(hours=1.55), 35), '.', transform=ccrs.PlateCarree(), color='red')
+    # ax2.plot(*simulation.satellite_at(1).track(time, time + datetime.timedelta(hours=24), 60), '.', transform=ccrs.PlateCarree(), color='blue')
+    results = simulation.bruteforce(time + datetime.timedelta(hours=3), time + datetime.timedelta(hours=3.3))
+    print(results[0])
 
-    for i in range(simulation.landmark_count()):
-        draw_landmark(ax2, simulation.landmark_at(i))
+    res_sim = vrc.from_bruteforce_result(simulation, results[0])
 
-    for i in range(simulation.station_count()):
-        draw_station(ax2, simulation.station_at(i))
+    print(res_sim.current_datetime())
 
-    ax2.plot(*simulation.satellite_at(0).track(time, time + datetime.timedelta(hours=1.55), 35), '.', transform=ccrs.PlateCarree(), color='red')
-    ax2.plot(*simulation.satellite_at(1).track(time, time + datetime.timedelta(hours=24), 60), '.', transform=ccrs.PlateCarree(), color='blue')
+    draw_simulation(ax2, res_sim)
     
     plt.show()
 
